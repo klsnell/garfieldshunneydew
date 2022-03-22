1ed7c8e1f6554fdf6edcf0f4eac84293c27b9c76
klsnell <Klsnell0351@gmail.com>
2022-03-21 20:58:27 -0400

    changed browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
+// import './App.css';
+// import React, { useState } from 'react';
+// import {BrowserRouter as Router, Routes, Route, useHistory} from 'react-router-dom';
+// import Navbar from './Components/NavBar/NavBar';
+// import Home from './Components/Home';
+// import Services from './Components/Services';
+// import Gallery from './Components/Gallery';
+// import Contact from './Components/Contact';
+// import Estimate from './Components/Estimate';
+// import Bars from './Components/Bar/Bars';
+// import Footer from './Components/Footer/Footer';
+
+
+// function App() {
+
+//   const [isOpen, setIsOpen] = useState(false)
+
+//   const toggle = () => {
+//       setIsOpen(!isOpen)
+//   }
+
+//   return (
+//     <Router>
+//       {/* <Home/> */}
+//       <Bars isOpen={isOpen} toggle={toggle}
+//         {...<Route path='*' exact element={<Home/>}/>}
+//         {...<Route path='/services' exact element={<Services/>}/>}
+//       />
+//             <Navbar toggle={toggle} />
+//       <Routes>
+//         <Route path='*' exact element={<Home/>} />
+//         <Route path='/services' exact element={<Services/>} />
+//         <Route path='/gallery' exact element={<Gallery/>} />
+//         <Route path='/contact' exact element={<Contact/>} />
+//         <Route path='/estimate' exact element={<Estimate/>} />
+//       </Routes> 
+//       <Footer/>
+//       <useHistory/>
+//     </Router>
+//   );
+// }
+
+// export default App;
+//!imports
+
+//?styled-components react-router-dom reactstrap react-grid-system pure-react-carousel react-icons react-scroll react-responsive-carousel react-alice-carousel
+
+
 import './App.css';
 import React, { useState } from 'react';
-import {BrowserRouter as Router, Routes, Route, useHistory} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './Components/NavBar/NavBar';
 import Home from './Components/Home';
 import Services from './Components/Services';
@@ -9,6 +57,7 @@ import Contact from './Components/Contact';
 import Estimate from './Components/Estimate';
 import Bars from './Components/Bar/Bars';
 import Footer from './Components/Footer/Footer';
+
 
 function App() {
 
@@ -23,7 +72,7 @@ function App() {
       {/* <Home/> */}
       <Bars isOpen={isOpen} toggle={toggle}
         {...<Route path='*' exact element={<Home/>}/>}
-        {...<Route path='/services' exact element={<Services/>}/>}
+        {...<Route path='#/services' exact element={<Services/>}/>}
       />
             <Navbar toggle={toggle} />
       <Routes>
@@ -40,6 +89,3 @@ function App() {
 }
 
 export default App;
-//!imports
-
-//?styled-components react-router-dom reactstrap react-grid-system pure-react-carousel react-icons react-scroll react-responsive-carousel react-alice-carousel
